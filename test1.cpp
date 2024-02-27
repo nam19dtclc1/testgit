421e8021f652077c14f2c4012ecf82dc9a06036e
nam19dtclc1 <ngocnam201220011@gmail.com>
2024-02-27 14:24:38 +0700

    add test

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
- int main(){
-  cout<<"aaaafagg";
+
+int main(){
+  int a,b,c;
+  cout<<"nhap a,b: ";
+  cin>>a>>b;
+  c=a+b;
+  cout<<"tong cua c la:"<<c;
   return 0;
- }
+}
